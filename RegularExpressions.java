8a51ec0b65c876e6cc290d6d7e7fe904cb60c244
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-21 15:21:53 +0530

    Add: As a user need to enter a valid last Name

--- RegularExpressions.java
+++ RegularExpressions.java
@@ -2,8 +2,12 @@ package regex;
 import java.util.regex.*;
 public class RegularExpressions{
 		public static void main(String[] args) {
-			Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
-			Matcher matcher = pattern.matcher("Chandrakala");
-			System.out.println("string matches the given Regex -"+matcher.matches());
+			Pattern firstName = Pattern.compile("^[A-Z][a-z]{3,}");//first name start with capital and has minimum 3 chararcters
+			Matcher first = firstName.matcher("Chandrakala");
+			Pattern lastName = Pattern.compile("^[A-Z][a-z]{3,}");
+			Matcher last = lastName.matcher("Mishra");
+
+			System.out.println("string matches the given Regex -"+first.matches());
+			System.out.println("string matches the given Regex -"+last.matches());
 }
 }
