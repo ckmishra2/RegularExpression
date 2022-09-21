4f4a03745491fa20bafb5e84decf4028157302a7
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-21 22:45:16 +0530

    Add: As a user need to follow pre-defined mobile format

--- RegularExpressions.java
+++ RegularExpressions.java
@@ -11,9 +11,13 @@ public class RegularExpressions {
 		Matcher last = lastName.matcher("Mishra");
 		Pattern emailP = Pattern.compile("^[a][b][c].[a-z]{3,}+@[b][l].[c][o].[a-z]{0,}");//for valid email
 		Matcher emailM = emailP.matcher("abc.chandrakala@bl.co.us");
+		Pattern mobileP = Pattern.compile("[0-9]{0,2}[1-9] [0-9]{10}");//for mobile format
+		Matcher mobileM = mobileP.matcher("91 8976544391");
+		
 		System.out.println("string matches the given Regex for first name -" + first.matches());
 		System.out.println("string matches the given Regex for last name -" + last.matches());
 		System.out.println("string matches the given Regex for email -" +  emailM.matches());
+		System.out.println("string matches the given Regex for mobile number -" +  mobileM.matches());
 		
 	}
 }
