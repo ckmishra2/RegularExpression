682c7276bb90e4d2e912c8b817dc0bf90cc0b111
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-21 22:39:58 +0530

    Add: Modify valid email

--- RegularExpressions.java
+++ RegularExpressions.java
@@ -9,14 +9,11 @@ public class RegularExpressions {
 		Matcher first = firstName.matcher("Chandrakala");
 		Pattern lastName = Pattern.compile("^[A-Z][a-z]{3,}"); //last name start with capital and has minimum 3 chararcters
 		Matcher last = lastName.matcher("Mishra");
+		Pattern emailP = Pattern.compile("^[a][b][c].[a-z]{3,}+@[b][l].[c][o].[a-z]{0,}");//for valid email
+		Matcher emailM = emailP.matcher("abc.chandrakala@bl.co.us");
+		System.out.println("string matches the given Regex for first name -" + first.matches());
+		System.out.println("string matches the given Regex for last name -" + last.matches());
+		System.out.println("string matches the given Regex for email -" +  emailM.matches());
 		
-		Pattern email = Pattern.compile("^[a-zA-Z.a-z0-9_!#$%'*&+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
-		Matcher email1 = email.matcher("abc.xyz@bl.co.in");
-		Pattern email2 = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
-		Matcher eml = email2.matcher("abc@gmail.com");
-		System.out.println("string matches the given Regex -" + first.matches());
-		System.out.println("string matches the given Regex -" + last.matches());
-		System.out.println("string matches the given Regex -" + eml.matches());
-		System.out.println("string matches the given Regex -" + email1.matches());
 	}
 }
