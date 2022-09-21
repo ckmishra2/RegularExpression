e0e8a1febdfac3abb2345156b7db9b282ec4c143
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-21 22:55:06 +0530

    Add: As a user need to follow predefined password rules

--- RegularExpressions.java
+++ RegularExpressions.java
@@ -4,8 +4,7 @@ import java.util.regex.*;
 
 public class RegularExpressions {
 	public static void main(String[] args) {
-		Pattern firstName = Pattern.compile("^[A-Z][a-z]{3,}");// first name start with capital and has minimum 3 chararcters
-																
+		Pattern firstName = Pattern.compile("^[A-Z][a-z]{3,}");// first name start with capital and has minimum 3 chararcters														
 		Matcher first = firstName.matcher("Chandrakala");
 		Pattern lastName = Pattern.compile("^[A-Z][a-z]{3,}"); //last name start with capital and has minimum 3 chararcters
 		Matcher last = lastName.matcher("Mishra");
@@ -13,11 +12,13 @@ public class RegularExpressions {
 		Matcher emailM = emailP.matcher("abc.chandrakala@bl.co.us");
 		Pattern mobileP = Pattern.compile("[0-9]{0,2}[1-9] [0-9]{10}");//for mobile format
 		Matcher mobileM = mobileP.matcher("91 8976544391");
-		
+		Pattern passwordP = Pattern.compile("[a-zA-Z]{8,}");//for Password
+		Matcher passwordM = passwordP.matcher("Abcretut");
 		System.out.println("string matches the given Regex for first name -" + first.matches());
 		System.out.println("string matches the given Regex for last name -" + last.matches());
 		System.out.println("string matches the given Regex for email -" +  emailM.matches());
 		System.out.println("string matches the given Regex for mobile number -" +  mobileM.matches());
+		System.out.println("string matches the given Regex for mobile number -" +  passwordM.matches());
 		
 	}
 }
